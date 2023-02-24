1ad727a2719f2b30b32b01b05b32de0dbb9e1b1d
shwetank <56911869+shwetank04@users.noreply.github.com>
2023-02-24 15:26:28 +0530

    Create admin.js

---
+++ 03-Dynamic Content - Templating Engines/routes/admin.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const path = require("path");
+const router = express.Router();
+
+const rootDir = require("../util/path");
+const products = [];
+router.get("/add-product", (req, res, next) => {
+  // res.sendFile(path.join(rootDir, "views", "add-product.html"));
+  res.render("add-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true,
+  });
+});
+
+router.post("/add-product", (req, res, next) => {
+  products.push({ title: req.body.title });
+  res.redirect("/");
+});
+
+exports.routes = router;
+exports.products = products;
