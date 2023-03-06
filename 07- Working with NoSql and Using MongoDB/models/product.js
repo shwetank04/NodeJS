bdec17f08ffa85963d01443728127a1260b7f305
shwetank <56911869+shwetank04@users.noreply.github.com>
2023-03-07 00:03:21 +0530

    Create product.js

---
+++ 07- Working with NoSql and Using MongoDB/models/product.js
@@ -0,0 +1,69 @@
+const mongodb = require("mongodb");
+const getDb = require("../util/database").getDb;
+
+class Product {
+  constructor(title, price, description, imageurl, id, userId) {
+    this.title = title;
+    this.price = price;
+    this.description = description;
+    this.imageUrl = imageurl;
+    this._id = id ? new mongodb.ObjectId(id) : null;
+    this.userId = userId;
+  }
+  save() {
+    const db = getDb();
+    let dbOp;
+    if (this._id) {
+      //update the product
+      dbOp = db
+        .collection("products")
+        .updateOne({ _id: new mongodb.ObjectId(this._id) }, { $set: this });
+    } else {
+      dbOp = db.collection("products").insertOne(this);
+    }
+    return dbOp
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((e) => console.log(e));
+  }
+
+  static fetchAll() {
+    const db = getDb();
+    return db
+      .collection("products")
+      .find()
+      .toArray()
+      .then((products) => {
+        console.log(products);
+        return products;
+      })
+      .catch((e) => console.log(e));
+  }
+
+  static findById(prodId) {
+    const db = getDb();
+    return db
+      .collection("products")
+      .find({ _id: new mongodb.ObjectId(prodId) })
+      .next()
+      .then((product) => {
+        console.log(product);
+        return product;
+      })
+      .catch((e) => console.log(e));
+  }
+
+  static deleteById(prodId) {
+    const db = getDb();
+    return db
+      .collection("products")
+      .deleteOne({ _id: new mongodb.ObjectId(prodId) })
+      .then((result) => {
+        console.log("Deleted");
+      })
+      .catch((e) => console.log(e));
+  }
+}
+
+module.exports = Product;
