2625f31589739f1d576e4c3d65758b26646b6b32
shwetank <56911869+shwetank04@users.noreply.github.com>
2023-03-08 22:59:15 +0530

    Create app.js

---
+++ 08- Working with Mongoose/app.js
@@ -0,0 +1,53 @@
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const errorController = require("./controllers/error");
+const User = require("./models/user");
+const mongoose = require("mongoose");
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res, next) => {
+  User.findById("640788e4e0183e7a41577660")
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+mongoose
+  .connect(
+    "mongodb+srv://shwetanksudhanshu:7YOOwxY185NqJD4A@cluster0.duq475d.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then((result) => {
+    User.findOne().then((user) => {
+      if (!user) {
+        const user = new User({
+          name: "Shwetank",
+          email: "shwshu@gmail.com",
+          cart: {
+            items: [],
+          },
+        });
+        user.save();
+      }
+    });
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
