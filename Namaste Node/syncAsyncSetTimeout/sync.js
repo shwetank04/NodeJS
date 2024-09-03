e0f1a9856978d000bf6d28d8ae206472cfbdf472
shwetank <56911869+shwetank04@users.noreply.github.com>
2024-09-03 19:45:40 +0530

    Create sync.js

---
+++ Namaste Node/syncAsyncSetTimeout/sync.js
@@ -0,0 +1,17 @@
+console.log("Hello World");
+
+var a = 107;
+var b = 209;
+
+function multiply(a,b){
+    const result = a * b;
+    return result;
+}
+
+var c = multiply(a,b);
+
+console.log("multiplication result is: ",c);
+
+//JS executes the code quickly as this gets dine with V8 engine alone.
+//Hello World
+// multiplication result is:  22363
