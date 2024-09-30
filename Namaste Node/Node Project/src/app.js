7ecc7b85d90dbfc4d83d81b77ae779cf931a5a78
shwetank <56911869+shwetank04@users.noreply.github.com>
2024-09-30 23:04:55 +0530

    Update app.js

--- Namaste Node/Node Project/src/app.js
+++ Namaste Node/Node Project/src/app.js
@@ -3,14 +3,26 @@ const express = require('express');
 //this is instance of express js application. Creating a new webserver using this.
 const app = express();
 
-//Request handler
-app.use("/",(req,res) =>{
-res.send("Hello Namaste from Server")
+//// If we put this Request handler here then if we go to / it will give Hello Namaste from Serve and if we do /test or anything else it will give 
+//Hello Namaste from Serve
+// app.use("/",(req,res) =>{
+// res.send("Hello Namaste from Server")
+// });
+
+//Adding route under Request handler. The /test will handle everything stating with /test. We can make it get.post.put. This current one will handle all /get.
+app.get("/test",(req,res) =>{
+    res.send("Hello from Server under Route Test")
 });
 
-//Adding route under Request handler
-app.use("/test",(req,res) =>{
-    res.send("Hello from Server under Route Test")
+app.get("/user",(req,res) =>{
+    res.send("Hello from Server under Route user")
+});
+
+// If we put this Request handler here then if we go to / it will give Hello Namaste from Serve and if we do /test it will give Hello from Server under Route Test.
+//The below route will handle all type of request type as its using use and if we use /user with POST and as there is nothing above with post then by default
+//below router will be called.
+app.use("/",(req,res) =>{
+res.send("Hello Namaste from Server")
 });
     
 
