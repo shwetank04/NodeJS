a37708848634b47f82e4472382c3a0d9f7776ecb
shwetank <56911869+shwetank04@users.noreply.github.com>
2024-10-02 11:28:28 +0530

    Update app.js

--- Namaste Node/Node Project Demo/src/app.js
+++ Namaste Node/Node Project Demo/src/app.js
@@ -9,6 +9,22 @@ const app = express();
 // res.send("Hello Namaste from Server")
 // });
 
+//If there is no res.send then the api will keep waiting until it gets timed out. 
+//Below code will send Response as output, as it goes to first route handler and sends back response and doesnt go any further.
+//If we comment first res.send then it wont go to next one, it will keep waiting. To make it go to next response handler, we need to use next.
+//If we have both res.send and next under first request handler, then it will still print only Response as response is already send but the code will still go to
+//next lines of code and goes to second route handler and console log will be printed but code will throw error as we are tryig to send other response 
+//in the same url.
+//We can put array of request handler as well.
+app.use('/test2',[(req,res,next)=>{
+    console.log("Handling the route user1");
+    // res.send("Response");
+    next();
+},(req,res) =>{
+    console.log("Handling the route user2");
+    res.send("2nd Response");
+}])
+
 //Adding route under Request handler. The /test will handle everything stating with /test. We can make it get.post.put. This current one will handle all /get.
 app.get("/test",(req,res) =>{
     res.send("Hello from Server under Route Test")
