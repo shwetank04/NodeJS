de3b5f6296830009c8fbd9ac5efbf2d991ff8660
shwetank <56911869+shwetank04@users.noreply.github.com>
2024-10-02 19:24:53 +0530

    Create ExceptionHandling.js

---
+++ Namaste Node/Node Project Demo/src/ExceptionHandling.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+
+
+app.get("/getUserData",(req,res)=>{
+    try{
+        throw new Error("Error");
+        res.send("User data sent")   
+    }
+    catch(err){
+        res.status(500).send("Some error occured")
+    }
+})
+
+//Here the err should be the first parameter. 
+app.use("/",(err,req,res,next)=> {
+    if(err){
+        console.error(err.message);
+        res.status(500).send("Something went wrong");
+    }
+})
+
+app.listen(3000,()=>{
+    console.log("Server is successfully listening on port 3000");
+});
