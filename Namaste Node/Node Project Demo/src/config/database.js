4fc36be3854c8e0afe3f9a11472d9259188151c5
shwetank <56911869+shwetank04@users.noreply.github.com>
2024-10-04 21:29:36 +0530

    Update database.js

--- Namaste Node/Node Project Demo/src/config/database.js
+++ Namaste Node/Node Project Demo/src/config/database.js
@@ -1,1 +1,6 @@
-db
+const mongoose = require('mongoose');
+const connectDb = async() =>{
+    await mongoose.connect("mongodb+srv://shwetanksudhanshu:kSUwBzDRxBOXbe6g@cluster0.duq475d.mongodb.net/")
+};
+
+module.exports = connectDb
