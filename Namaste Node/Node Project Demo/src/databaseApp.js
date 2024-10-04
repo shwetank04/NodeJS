446bd00030b28f67f44013763d57c112c8df407d
shwetank <56911869+shwetank04@users.noreply.github.com>
2024-10-04 22:53:30 +0530

    Update databaseApp.js

--- Namaste Node/Node Project Demo/src/databaseApp.js
+++ Namaste Node/Node Project Demo/src/databaseApp.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const connectDb = require('./config/database');
+const User = require('./models/user')
 const app = express();
 
 
+app.post('/signup', async (req,res) => {
+
+    //creating a new user with above data.
+    //creating a new instance of User model
+    const user = new User({
+        firstName:"shwetank",
+        lastName:"sudhanshu",
+        emailId:"shw@gmailcom",
+        password:"shw123"
+    })
+
+    await user.save();
+    res.send("User saved successfully")
+})
+
+
+//Connecting to db and then only starting the application
 connectDb().then(() =>{
     console.log("Connected to DB");
     app.listen(3000,()=>{
@@ -12,3 +30,4 @@ connectDb().then(() =>{
 .catch((err) => {
     console.log("Error in connecting to DB "+err)
 });
+
