73aad5ce688f45e5ab2cb7897709155de06695da
shwetank <56911869+shwetank04@users.noreply.github.com>
2024-10-07 19:05:26 +0530

    Update dataSanitizationAndValidation.js

--- Namaste Node/Node Project Demo/src/dataSanitizationAndValidation.js
+++ Namaste Node/Node Project Demo/src/dataSanitizationAndValidation.js
@@ -1,1 +1,106 @@
-//Initial commit 
+const express = require('express');
+const connectDb = require('./config/database');
+const User = require('./models/user')
+const app = express();
+
+//Middleware for reading JSON request for all incoming request
+app.use(express.json());
+
+//Get all user from database
+app.get('/feed',async (req,res)=>{
+    try{
+        const users = await User.find();
+        if(users.length === 0){
+            res.status(404).send("No Users found");
+        }
+        else{
+            res.send(users);
+        }    }
+    catch(err){
+        console.log(err);
+        res.status(500).send(err);
+    }
+})
+
+//Get user based on email.
+app.get("/user", async (req,res)=>{
+    const userEmail = req.body.emailId;
+    try{
+        const users = await User.find({emailId: userEmail});
+        if(users.length === 0){
+            res.status(404).send("User not found");
+        }
+        else{
+            res.send(users);
+        }
+    }
+    catch(err){
+        console.log(err);
+        res.status(500).send(err);
+    }
+})
+
+app.post('/signup', async (req,res) => {
+
+    //If we do req.body it will give undefined as our server is not able to read the JSON data. So we added above express.json
+    const user = new User(req.body);
+    try{
+        await user.save();
+        res.send("User saved successfully")
+    }
+    catch(err){
+        console.log(err);
+        res.status(500).send(err);
+    }
+})
+
+//delete a user
+app.post("/deleteUser",async (req,res)=>{
+    const userId = req.body.userId;
+    try{
+        const user = await User.findByIdAndDelete(userId);
+        res.send("User deleted successfully");
+    }
+    catch(err){
+        console.log(err);
+        res.status(500).send(err);
+    }
+})
+
+
+//Update data of the user
+app.patch("/updateUser/:userId",async (req,res) => {
+    const userId = req.params?.userId;
+    const data = req.body;
+    const allowedUpdates = ['about','gender','age','skills'];
+    try{
+        const isUpdateAllowed = Object.keys(data).every(input =>
+            allowedUpdates.includes(input)
+        )
+        if(!isUpdateAllowed){
+            throw new Error("Update not allowed");
+        }
+        //return document which was before update. If we put after it will return the document after the update.
+        //run validators is use to run the validation function from model in patch related operations
+        const before = await User.findByIdAndUpdate({_id:userId},data,{returnDocument : "before",runValidators:true});
+        console.log("Before document "+before)
+        res.send("User updated successfully");
+    }
+    catch(err){
+        console.log(err);
+        res.status(500).send(err.message);
+    }
+})
+
+
+//Connecting to db and then only starting the application
+connectDb().then(() =>{
+    console.log("Connected to DB");
+    app.listen(3000,()=>{
+        console.log("Server is successfully listening on port 3000");
+    });
+})
+.catch((err) => {
+    console.log("Error in connecting to DB "+err)
+});
+
