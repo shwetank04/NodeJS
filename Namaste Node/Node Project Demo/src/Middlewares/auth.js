07dd100d8156e2b6f3e7814eee741309d433375a
shwetank <56911869+shwetank04@users.noreply.github.com>
2024-10-12 11:54:41 +0530

    Update auth.js

--- Namaste Node/Node Project Demo/src/Middlewares/auth.js
+++ Namaste Node/Node Project Demo/src/Middlewares/auth.js
@@ -1,3 +1,6 @@
+const jwt = require("jsonwebtoken");
+const User = require('../models/user');
+
 //MiddleWare which only works for api starting with /admin
 const adminAuth = (req,res,next)=>{
     const token = 'xyz'
@@ -10,6 +13,31 @@ const adminAuth = (req,res,next)=>{
     }
 }
 
+//Middleware for token authentication
+const tokenAuth = async (req,res,next) => {
+    try{
+    //Read the Token
+    const {token} = req.cookies;
+    if (!token) {
+        throw new Error("Invalid Token");
+    }
+    //Validate the Token
+    const decodedMsg = await jwt.verify(token,"NODEPROJECT$790");
+    //Find the User
+    const {_id} = decodedMsg;
+    const user = await User.findById(_id);
+    if(!user){
+        throw new Error("Invalid User");
+    }
+    req.user = user;
+    next();
+    }
+    catch(err){
+        console.log(err);
+        res.status(500).send(err.message)
+    }
+}
+
 module.exports ={
-    adminAuth
+    adminAuth,tokenAuth
 }
