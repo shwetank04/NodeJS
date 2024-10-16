1602bd85fb9514e0d08b29f27cc7a7b8211acf38
shwetank <56911869+shwetank04@users.noreply.github.com>
2024-10-16 23:14:46 +0530

    Update validation.js

--- Namaste Node/Node Project Demo/src/utils/validation.js
+++ Namaste Node/Node Project Demo/src/utils/validation.js
@@ -21,4 +21,12 @@ const validateSignUpData = (req) => {
     }
 }
 
-module.exports = validateSignUpData;
+const validateProfileEditData = (req) => {
+    const allowedEditFields = ["firstName","lastName","emailId","photoUrl","gender","age","about","skills"];
+    const isEditAllowed = Object.keys(req.body).every(field => allowedEditFields.includes(field));
+    if(!isEditAllowed){
+        throw new Error("Validation Failed");
+    }
+}
+
+module.exports = {validateSignUpData,validateProfileEditData};
