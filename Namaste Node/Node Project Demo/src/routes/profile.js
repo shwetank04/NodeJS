f817c3500cd08156bfaceffa0706b32b0866f941
shwetank <56911869+shwetank04@users.noreply.github.com>
2024-10-16 23:33:06 +0530

    Update profile.js

--- Namaste Node/Node Project Demo/src/routes/profile.js
+++ Namaste Node/Node Project Demo/src/routes/profile.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const profileRouter = express.Router();
 const {tokenAuth} = require('../middleware/auth');
+const {validateProfileEditData} = require('../utils/validation');
 
-profileRouter.get("/profile",tokenAuth,async (req,res) => {
+profileRouter.get("/profile/view",tokenAuth,async (req,res) => {
     try{
         res.status(200).send(req.user ? req.user : "User not found");
+    }
+    catch(err){
+        res.status(500).send(err.message);
+    }
+});
+
+profileRouter.patch("/profile/edit",tokenAuth,async (req,res) => {
+    try{
+        validateProfileEditData(req);
+        //Token auth has attached the user in req
+        const user = req.user;
+        Object.keys(req.body).forEach(key => user[key] = req.body[key]);
+        await user.save();
+        res.status(200).send("Updated Successfully");
     }
     catch(err){
         res.status(500).send(err.message);
