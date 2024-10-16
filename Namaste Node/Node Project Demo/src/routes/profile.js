a57160799bf18a3f276b7b8e0eb1695d9c8e7d11
shwetank <56911869+shwetank04@users.noreply.github.com>
2024-10-17 00:10:25 +0530

    Update profile.js

--- Namaste Node/Node Project Demo/src/routes/profile.js
+++ Namaste Node/Node Project Demo/src/routes/profile.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const profileRouter = express.Router();
 const {tokenAuth} = require('../middleware/auth');
-const {validateProfileEditData} = require('../utils/validation');
+const {validateProfileEditData,validateProfilePasswordData} = require('../utils/validation');
+const bcrypt = require('bcrypt');
 
 profileRouter.get("/profile/view",tokenAuth,async (req,res) => {
     try{
@@ -20,6 +21,26 @@ profileRouter.patch("/profile/edit",tokenAuth,async (req,res) => {
         Object.keys(req.body).forEach(key => user[key] = req.body[key]);
         await user.save();
         res.status(200).send("Updated Successfully");
+    }
+    catch(err){
+        res.status(500).send(err.message);
+    }
+});
+
+profileRouter.patch("/profile/password",tokenAuth,async (req,res) => {
+    try{
+        const currentPassword = req.body.currentPassword;
+        const user = req.user;
+        const isPasswordValid = user.validatePassword(currentPassword);
+        if(isPasswordValid) {
+            const newPassword = await bcrypt.hash(req.body.newPassword,10);
+            user.password = newPassword;
+            await user.save();
+            res.status(200).send("Password Updated Successfully");
+        }
+        else{
+            throw new Error("Invalid current password")
+        }
     }
     catch(err){
         res.status(500).send(err.message);
