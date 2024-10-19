a87b8c3454e61acb57c9fd58f6b4f8a50275dfec
shwetank <56911869+shwetank04@users.noreply.github.com>
2024-10-19 19:35:24 +0530

    Update request.js

--- Namaste Node/Node Project Demo/src/routes/request.js
+++ Namaste Node/Node Project Demo/src/routes/request.js
@@ -1,10 +1,50 @@
 const express = require('express');
 const requestRouter = express.Router();
 const {tokenAuth} = require('../middleware/auth');
+const ConnectionRequest = require('../models/connectionRequest');
+const User = require('../models/user');
 
+requestRouter.post("/request/send/:status/:toUserId",tokenAuth, async (req,res) => {
+    try{
+        const fromUserId = req.user._id;
+        const toUserId = req.params.toUserId;
+        const status = req.params.status;
 
-requestRouter.post("/sendConnectionRequest",tokenAuth, (req,res) => {
-    res.send("Connection request send");
+        const allowedStatus = ["ignored","interested"];
+        if(!allowedStatus.includes(status)){
+            throw new Error(status +" status type not allowed");
+
+        }
+        const toUser = await User.findById(toUserId);
+        if(!toUser){
+            throw new Error("Invalid user Id");
+        }
+        //Check if there is an existing ConnectionRequest, from one user to other and vice versa
+        const existingConnectionRequest = await ConnectionRequest.findOne({
+            $or: [
+                {fromUserId, toUserId},
+                {fromUserId : toUserId, toUserId: fromUserId}
+            ]
+        });
+
+        if(existingConnectionRequest){
+            return res.status(400).send("Connection Request already exists");
+        }
+        const connectionRequest = new ConnectionRequest({
+            fromUserId,
+            toUserId,
+            status
+        })
+        const data = await connectionRequest.save();
+
+        res.json({
+            message: "Connection Request Sent Successfully",
+            data : data
+        })
+    }
+    catch(e){
+        res.status(400).send(e.message);
+    }
 });
 
 module.exports = requestRouter;
