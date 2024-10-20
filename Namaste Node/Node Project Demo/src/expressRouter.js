aa1585b0628c3d615a80ea553fe3c1d2af718901
shwetank <56911869+shwetank04@users.noreply.github.com>
2024-10-20 14:29:22 +0530

    Update expressRouter.js

--- Namaste Node/Node Project Demo/src/expressRouter.js
+++ Namaste Node/Node Project Demo/src/expressRouter.js
@@ -4,6 +4,7 @@ const authRouter = require('./routes/auth');
 const profileRouter = require('./routes/profile');
 const requestRouter = require('./routes/request');
 const cookieParser = require('cookie-parser');
+const userRouter = require('./routes/user');
 const app = express();
 
 //Middleware for reading JSON request for all incoming request
@@ -14,6 +15,7 @@ app.use(cookieParser());
 app.use('/',authRouter);
 app.use('/',profileRouter);
 app.use('/',requestRouter);
+app.use('/',userRouter);
 
 
 //Connecting to db and then only starting the application
