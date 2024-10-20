88a5fc450b5eed854000a4be81c4dcd8d6e53cc1
shwetank <56911869+shwetank04@users.noreply.github.com>
2024-10-20 15:56:07 +0530

    Update user.js

--- Namaste Node/Node Project Demo/src/routes/user.js
+++ Namaste Node/Node Project Demo/src/routes/user.js
@@ -22,4 +22,21 @@ userRouter.get('/user/requests/received',tokenAuth, async(req,res) => {
     }
 })
 
+//Information on who has accepted the request and who all are my connections. Connections can be either sender or receiver.
+userRouter.get('/user/connections',tokenAuth,async(req,res)=>{
+    try{
+        const loggedInUser = req.user;
+        const connectionRequest  = await ConnectionRequest.find({
+            $or: [
+                {toUserId: loggedInUser._id, status: "accepted"},
+                {fromUserId: loggedInUser._id, status: "accepted"}
+            ]
+        }).populate("fromUserId",["firstName"]).populate("toUserId",["firstName"]);
+        res.json({data: connectionRequest});
+    }
+    catch(e){
+        res.status(500).send(e.message);
+    }
+})
+
 module.exports = userRouter;
